92aa683b174d1888df6817baef4b9b31adc862dd
Valentina Acosta <valen.acosta19@hotmail.com>
2023-01-24 21:29:59 -0300

    feat: add styled components to navbar and product list

--- platzi-nextjs/components/Navbar.tsx
+++ platzi-nextjs/components/Navbar.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  background-color: #f3edda;
+  padding: 10px;
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const Navbar = () => {
   return (
-      <nav>
-          <Link href='/'>
-            Home
-          </Link>
-          <Link href='/about'>
-            About
-          </Link>
-      </nav>
-  )
-}
+    <Nav>
+      <Flex>
+        <Link href="/">
+          <Image alt="logo" src="/logo.jpg" width={100} height={100} />
+        </Link>
+        <Link href="/about">About</Link>
+      </Flex>
+    </Nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- platzi-nextjs/components/ProductList.tsx
+++ platzi-nextjs/components/ProductList.tsx
@@ -1,22 +1,45 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import styled from "styled-components";
+
+export const Flex = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  row-gap: 2rem;
+`;
+
+export const Card = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  position: relative;
+  &:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const TextContainer = styled.div`
+  padding: 2px 16px;
+`;
 
 const ProductList = ({ productList }: { productList: TProduct[] }) => {
   return (
-    <div>
+    <Flex>
       {productList.map(({ id, image, name, price }) => {
         return (
           <Link key={id} href={`/product/${id}`}>
-            <div>
-              <Image src={image} alt={name} width="333" height="333" />
-              <h2>{name}</h2>
-              <p>{price}</p>
-            </div>
+            <Card>
+              <Image src={image} alt={name} width={300} height={300} />
+              <TextContainer>
+                <h2>{name}</h2>
+                <p>{price}</p>
+              </TextContainer>
+            </Card>
           </Link>
         );
       })}
-    </div>
+    </Flex>
   );
 };
 
